100294ac5f99291afb01adabd290b7a1c14a2653
kutluhanyegit-pilsanpc <kutluhanyegit@icloud.com>
2025-03-24 13:19:06 +0300

    005 - DependencyInjection - Autofac

---
+++ 005 - DependencyInjection - Autofac/Application/DependencyInjections/Autofac/AutofacModule.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.Mapping;
+using Application.Services;
+using Autofac;
+using AutoMapper;
+using Domain.Interfaces.UnitOfWork;
+using Infrastructure.Contexts.EntityFramework;
+using Infrastructure.Identity.Hasher;
+using Infrastructure.Identity.Jwt;
+using Infrastructure.Repositories.ClaimRepository;
+using Infrastructure.Repositories.RoleRepository;
+using Infrastructure.Repositories.UserRepository;
+using Infrastructure.Repositories.UserRoleRepository;
+using Infrastructure.UnitOfWork;
+
+namespace Application.DependencyInjections.Autofac
+{
+    public class AutofacModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Application DI
+            builder.RegisterType<RoleService>().As<IRoleService>().InstancePerLifetimeScope();
+            builder.RegisterType<UserService>().As<IUserService>().InstancePerLifetimeScope();
+            builder.RegisterType<AuthService>().As<IAuthService>().InstancePerLifetimeScope();
+            builder.RegisterType<UserRoleService>().As<IUserRoleService>().InstancePerLifetimeScope();
+            builder.RegisterType<ClaimService>().As<IClaimService>().InstancePerLifetimeScope();
+            
+
+
+            //Infrastructure DI
+            builder.RegisterType<RoleRepository>().As<IRoleRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<UserRoleRepository>().As<IUserRoleRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<ClaimRepository>().As<IClaimRepository>().InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+
+            builder.RegisterType<PasswordHasher>().As<IPasswordHasher>().InstancePerLifetimeScope();
+            builder.RegisterType<TokenService>().As<ITokenService>().InstancePerLifetimeScope();
+            
+
+            //DbContext DI
+            builder.RegisterType<AppDbContext>().AsSelf().InstancePerLifetimeScope();
+
+            //AutoMapper
+            builder.Register(config => new MapperConfiguration( c => {
+                //Add Mapper Profile
+                c.AddProfile(new RoleProfile());
+                c.AddProfile(new UserProfile());
+                c.AddProfile(new UserRoleProfile());
+                c.AddProfile(new ClaimProfile());
+            })).AsSelf().SingleInstance();
+
+            builder.Register( ctx => ctx.Resolve<MapperConfiguration>().CreateMapper() ).As<IMapper>().InstancePerLifetimeScope();
+            
+
+
+        }
+    }
+}
